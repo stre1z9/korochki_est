6dc4e5524f38bd85f322b97c2101c3682abefe28
stre1z9 <114341458+stre1z9@users.noreply.github.com>
2026-05-13 11:02:59 +0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 class Config:
     SECRET_KEY = 'super_secret_key'
-    SQLALCHEMY_DATABASE_URI = ('postgresql://postgres:root@localhost/korochki_est')
+    SQLALCHEMY_DATABASE_URI = ('postgresql://korochki_db_2y6o_user:37UR68AnVDMFH6sYc7SuTTxu7j4MtkKq@dpg-d821surbc2fs73bv6tfg-a/korochki_db_2y6o')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
